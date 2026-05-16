9a770d8dff37fb339bb56d23ed9f74f9021bdcc6
Ermiyas Misiker <89203924+ermizamr@users.noreply.github.com>
2026-05-17 01:07:18 +0300

    Add main App component and all page sections

---
+++ src/app/App.tsx
@@ -0,0 +1,23 @@
+import HeroSection from "./components/HeroSection";
+import LoveLetterSection from "./components/LoveLetterSection";
+import MemoriesGallery from "./components/MemoriesGallery";
+import InteractiveSection from "./components/InteractiveSection";
+import IntimatePhoto from "./components/IntimatePhoto";
+import SurpriseSection from "./components/SurpriseSection";
+import FinalSection from "./components/FinalSection";
+import FloatingHearts from "./components/FloatingHearts";
+
+export default function App() {
+  return (
+    <div className="size-full overflow-y-auto overflow-x-hidden scroll-smooth">
+      <FloatingHearts />
+      <HeroSection />
+      <LoveLetterSection />
+      <MemoriesGallery />
+      <InteractiveSection />
+      <IntimatePhoto />
+      <SurpriseSection />
+      <FinalSection />
+    </div>
+  );
+}
